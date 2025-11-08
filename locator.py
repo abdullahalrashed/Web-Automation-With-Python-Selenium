f52a340864a6f9aa3cfafbedc6e59ea0ea77145e
raashare <raashare1@gmail.com>
2025-11-08 21:16:12 +0600

    link text, partial link text, tag, class

--- locator.py
+++ locator.py
@@ -17,6 +17,16 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()
-driver.find_element(By.ID, "small-searchterms").send_keys("Lenovo Thinkpad Carbon Laptop")
+#id and name locators
+# driver.find_element(By.ID, "small-searchterms").send_keys("Lenovo Thinkpad Carbon Laptop")
+
+#link text and partial link test
+
+#driver.find_element(By.LINK_TEXT,"Register").click()
+# driver.find_element(By.PARTIAL_LINK_TEXT,"Register").click()
+
+# sliders = driver.find_elements(By.CLASS_NAME, "slider_img")
+# print(len(sliders))
+
 
 time.sleep(5)
