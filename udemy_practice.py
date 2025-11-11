8f8c356d81a62aa6e75d36401732afe1344fe308
raashare <raashare1@gmail.com>
2025-11-12 02:26:01 +0600

    Basic calculator operation

--- udemy_practice.py
+++ udemy_practice.py
@@ -23,27 +23,71 @@
 
 ###################### constructior and types of variable #########################3
 
-class Calculator:
-    num = 100
+# class Calculator:
+#     num = 100
+#
+#     def __init__(self, a, b):
+#         self.first_number = a
+#         self.second_number = b
+#         print("I am called automatically when an object is called")
+#
+#     def get_data(self):
+#         print("I am now executing a method in class")
+#
+#     def summation(self):
+#         return self.first_number + self.second_number + self.num
+#
+# obj = Calculator(2, 3)
+# obj.get_data()
+# print(obj.summation())
+#
+# obj1 = Calculator(4, 5)
+# obj1.get_data()
+# print(obj1.summation())
 
-    def __init__(self, a, b):
-        self.first_number = a
-        self.second_number = b
-        print("I am called automatically when an object is called")
 
-    def get_data(self):
-        print("I am now executing a method in class")
 
-    def summation(self):
-        return self.first_number + self.second_number + self.num
+############################## BASIC CALCULATOR CALCULATION ################################
 
-obj = Calculator(2, 3)
-obj.get_data()
-print(obj.summation())
+class BasicCalculator:
 
-obj1 = Calculator(4, 5)
-obj1.get_data()
-print(obj1.summation())
+    def __init__(self, num1, num2):
+        self.first_number = num1
+        self.second_number = num2
+
+    def addition(self):
+        return self.first_number + self.second_number
+
+    def subtraction(self):
+        return self.first_number - self.second_number
+
+    def multiplication(self):
+        return self.first_number * self.second_number
+
+    def division(self):
+        return self.first_number / self.second_number
+
+
+obj = BasicCalculator(10, 5)
+obj.addition()
+obj.subtraction()
+obj.multiplication()
+obj.division()
+
+print("Addition: 10+5 = ", obj.addition())
+print("Subtraction: 10 - 5 = ", obj.subtraction())
+print("Multiplication: 10*5 = ", obj.multiplication())
+print("Division: 10/5 = ", obj.division())
+
+
+
+
+
+
+
+
+
+
 
 
 
