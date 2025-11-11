70c58c9e6d459180026ab37db2a5c27b1c6692ca
raashare <raashare1@gmail.com>
2025-11-11 22:51:01 +0600

    dictonary

---
+++ udemy practice.py
@@ -0,0 +1,11 @@
+# # how to save files in the dictionary
+#
+# dict = {}
+#
+# dict[ "firstname" ] = "Abdullah Al"
+# dict["Lastname"] = "Rashed"
+# dict["gender"] = "Male"
+# dict["age"] = 25
+#
+# print(dict)
+
