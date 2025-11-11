91e3edba467adbf593896f674720d2840fc38081
raashare <raashare1@gmail.com>
2025-11-12 03:05:41 +0600

    Basic calculator operation

--- child inheretence.py
+++ child inheretence.py
@@ -12,3 +12,20 @@ class childinl(Calculator):
 obj = childinl()
 obj.get_complete_data()
 print(obj.get_complete_data())
+
+
+# 1. Create a function called CalculateAverage that takes three parameters
+def CalculateAverage(num1, num2, num3):
+    # Calculate the sum of the three numbers
+    total = num1 + num2 + num3
+
+    average = total / 3
+    return average
+
+input_num1 = 10
+input_num2 = 20
+input_num3 = 30
+
+result = CalculateAverage(input_num1, input_num2, input_num3)
+
+print(f"The average of {input_num1}, {input_num2}, and {input_num3} is {result}")
