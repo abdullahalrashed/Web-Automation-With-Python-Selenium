1f7eb1bbd4d16124493a860e33a67c7fe318cb2d
raashare <raashare1@gmail.com>
2025-11-14 01:58:54 +0600

    hideable box and non dynamic checkbox

--- unacademy.py
+++ unacademy.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
@@ -82,6 +83,27 @@ for checkbox in checkboxes:
         checkbox.click()
         assert checkbox.is_selected()
         break
+
+############### hideable and non dynamic check box
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+checkboxes = driver.find_elements(By.XPATH, "//input[@type = 'checkbox'] ")
+print(len(checkboxes))
+for checkbox in checkboxes:
+    if checkbox.get_attribute("value") == "option2":
+        checkbox.click()
+        assert checkbox.is_selected()
+        break
+
+radiobuttons = driver.find_elements(By.CSS_SELECTOR, ".radioButton")
+radiobuttons[2].click()
+assert radiobuttons[2].is_selected()
+
+assert driver.find_element(By.ID, "displayed-text").is_displayed()
+driver.find_element(By.ID, 'hide-textbox').click()
+assert not driver.find_element(By.ID, "displayed-text").is_displayed()
+
 
 time.sleep(3)
 driver.quit()
