287e31cb1f01f062df2046e5d6ef71996f0613b8
raashare <raashare1@gmail.com>
2025-11-13 21:20:22 +0600

    CHANGE PASSWORD

--- unacademy.py
+++ unacademy.py
@@ -27,4 +27,32 @@ message = driver.find_element(By.CLASS_NAME, "alert-success").text
 print(message)
 assert "Success" in message
 time.sleep(5)
+
+driver.get("https://rahulshettyacademy.com/client/#/auth/login")
+
+driver.find_element(By.LINK_TEXT, "Forgot password?").click()
+driver.find_element(By.XPATH, "//form/div[1]/input").send_keys("raashare17092@gmail.com")
+driver.find_element(By.CSS_SELECTOR, "form div:nth-child(2) input").send_keys("@Password1234")
+driver.find_element(By.CSS_SELECTOR, "form div:nth-child(3) input").send_keys("@Password1234")
+driver.find_element(By.XPATH, "//button[text()= 'Save New Password']").click()
+time.sleep(3)
+
+driver.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+time.sleep(5)
+
 driver.close()
